af70bcfc7857c536221bd8c8d3674bb742e26eb1
Ahmadamer1 <s12011017@stu.najah.edu>
2023-05-11 07:42:52 +0200

    React-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { API, Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
-function App() {
+async function getIdToken() {
+  const currentUser = await Auth.currentAuthenticatedUser();
+  return currentUser.signInUserSession.idToken.jwtToken;
+}
+const cors=require("cors");
+function FileList() {
+  const [files, setFiles] = useState([]);
+  const [showFiles, setShowFiles] = useState(false);
+
+  useEffect(() => {
+    if (showFiles) {
+      fetchFiles();
+    }
+  }, [showFiles]);
+
+  async function fetchFiles() {
+    try {
+      const response = await API.get('Ahmad-API', '/files');
+      const body = JSON.parse(response.body);
+      const fileList = Array.isArray(body) ? body : body.files;
+      console.log('Files retrieved:', fileList);
+      setFiles(fileList);
+    } catch (error) {
+      console.error('Error fetching files:', error);
+    }
+  }
+
+  async function downloadFile(fileName) {
+    try {
+      const response = await API.get('Ahmad-API', `/files/${fileName}`, {
+        responseType: 'blob',
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', fileName);
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.error('Error downloading file:', error);
+    }
+  }
+
+  async function deleteFile(fileName) {
+    try {
+      const idToken = await getIdToken();
+      const myInit = {
+        headers: {
+          Authorization: idToken,
+        },
+      };
+      await API.del('Ahmad-API', `/files/${fileName}`, myInit);
+      setFiles((prevFiles) => prevFiles.filter((file) => file !== fileName));
+    } catch (error) {
+      console.error('Error deleting file:', error);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Files List</h2>
+      <button onClick={() => setShowFiles(!showFiles)}>
+        {showFiles ? 'Hide Files' : 'Show Files'}
+      </button>
+      {showFiles && files && (
+  <ul>
+    {files.map((file) => (
+      <li key={file.name}>
+        {file.name}
+        <button onClick={() => downloadFile(file.name)}>Download</button>
+        <button onClick={() => deleteFile(file.name)}>Delete</button>
+      </li>
+    ))}
+  </ul>
+)}
     </div>
   );
 }
 
-export default App;
+function FileUploader() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  async function handleFileChange(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+  
+    try {
+      const fileContent = await readFileAsDataURL(file);
+      setSelectedFile({
+        name: file.name,
+        content: fileContent
+      });
+    } catch (error) {
+      console.error('Error reading file:', error);
+    }
+  }
+  
+  function readFileAsDataURL(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
+  }
+
+  async function uploadFile() {
+    if (!selectedFile) return;
+  
+    const requestBody = {
+      fileName: selectedFile.name,
+      csv: selectedFile.content,
+    };
+  
+    try {
+      await API.post('Ahmad-API', '/files', {
+        body: requestBody,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      setSelectedFile(null);
+      alert('File uploaded successfully');
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadFile}>Upload File</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <FileList />
+      <FileUploader />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
